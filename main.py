21495abf94be37555973427ca6b9337afed6f569
PS <suchodolskipatryk@gmail.com>
2023-12-12 20:20:44 +0100

    Downloader ver. 0.5 functional, for Gambler magazine only

---
+++ main.py
@@ -0,0 +1,84 @@
+import bs4
+import requests
+import os
+
+
+# loop for downloading all issues from given year
+def download_whole_year(year):
+    number = 1
+    y = int(year)
+    directory = os.path.expanduser("~") + "\\Desktop\\"
+
+    while True:
+        try:
+            name = "Gambler_{year}_{number}".format(
+                number=str(number).zfill(2), year=y)
+
+            # url pattern for Gambler
+            base_url = requests.get(
+                'https://archive.org/details/gambler_magazine-{year}-{number}'.format(
+                    year=y, number=str(number).zfill(2)))
+            soup = bs4.BeautifulSoup(base_url.text, 'lxml')
+            # find pdf download link in soup
+            pdf_url = soup.find_all("a",
+                                    {"class": "format-summary download-pill"})
+            # pdf download link pattern, 3rd index in download links
+            download_url = 'https://archive.org{}'.format(
+                pdf_url[3].get('href'))
+
+            # save as .pdf
+            try:
+                # directory exists check
+                download_directory = os.mkdir(
+                    os.path.join(directory, "Gambler\\"))
+            except FileExistsError:
+                pass
+            finally:
+                os.chdir(directory + "Gambler")
+
+                # file exists check
+                if os.path.isfile(
+                        directory + "Gambler\\" + "{name}.pdf".format(
+                                name=name)):
+                    # uncompleted download check
+                    try:
+                        if os.path.getsize(
+                                os.path.join(os.getcwd(), name + ".pdf")) == 0:
+                            os.remove(os.path.join(os.getcwd(), name + ".pdf"))
+                            continue
+                    except FileNotFoundError:
+                        pass
+                    # completed download check
+                    else:
+                        print(f"Issue {number} / {y} already downloaded")
+                        number += 1
+                # write file
+                else:
+                    with open("{name}.pdf".format(name=name), 'wb') as file:
+                        file.write(requests.get(download_url).content)
+                    print(f"Issue {number} / {y} downloaded")
+                    number += 1
+
+        except IndexError:
+            # searching for first issue from first year <== change print()
+            if number < 12:
+                print("Searching for first issue...\n")
+                number += 1
+                continue
+            # check for full year ==> number > 12 for monthly magazine
+            else:
+                print(f"All issues from year {y} downloaded")
+                break
+
+
+def download_all(years):
+    all_years = years
+
+    for year in all_years:
+        download_whole_year(year)
+
+    print("Download completed")
+
+
+download_all([1993, 1994, 1995, 1996, 1997,
+              1998, 1999])
